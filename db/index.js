f64af17aab0bfb1758aac925200194b99afece0c
unknown <you@example.com>
2022-08-27 06:58:01 -0400

    first commit

--- db/index.js
+++ db/index.js
@@ -2,11 +2,13 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  user: 'xvbgavtchsxpue',
-  host: 'ec2-44-205-112-253.compute-1.amazonaws.com',
-  database: 'dftp0851t8cp9l',
-  password: 'e3364231965fadb23da3d6fb9c2cf18a653d431df6c90926dfd5602f6cc7e958',
-});
+  user: 'postgres',
+  host: 'localhost',
+  database: 'yelp',
+  password: 'Kubin200519%',
+  port: 5432,
+})
+
 export default {
   query: (text, params) => pool.query(text, params),
 };
--- server.js
+++ server.js
@@ -9,10 +9,7 @@ import db from "./db/index.js";
 const app = express();
 
 //MIDDLEWARE
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  next();
-});
+app.use(cors());
 app.use(express.json());
 
 //PORT LISTEN
